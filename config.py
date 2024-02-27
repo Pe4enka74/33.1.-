23d0914b642c912525310cffae8b191c982e0abf
Pe4enka74 <136488062+Pe4enka74@users.noreply.github.com>
2024-02-27 22:25:52 +0500

    config.py

--- config.py
+++ config.py
@@ -6,13 +6,13 @@ class RostelecomInfo():
     URL_Online = 'https://my.rt.ru/'
     URL_SmartHome = 'https://lk.smarthome.rt.ru/'
 
-    login = "rtkid_1696533610038"
+    login = "rtkid_1709054403303"
     invalid_login = "rtkid_0000000000000"
-    mail = "wewike5179@gronasu.com"
+    mail = "nikitakonovalov174rus@gmail.com"
 
 
-    valid_password1 = "5YsNuzR62"
-    password2 = "5YsNuzR61"
+    valid_password1 = "Gtxtymrf74"
+    password2 = "Gtxtymrf74rus"
 
     tab_phone_text = "Номер"
     tab_mail_text = "Почта"
